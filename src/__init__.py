2d4d82ba828e090b908d7c9515c2915aa7181521
shaanpatel-data <shaanpatel@gmail.com>
2026-03-11 10:55:28 -0700

    Add src/__init__.py with package initialization docstring

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Initialization module for the parlay optimization package.
+
+This package provides modules to construct and evaluate multi-leg sports outcome
+parlay combinations and optimize them based on expected value metrics.
+
+See README.md for an overview of the project.
+"""
