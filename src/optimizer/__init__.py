7fd25f2b04679fad2978ef1f032c6ec29ff093a8
shaanpatel-data <shaanpatel@gmail.com>
2026-03-11 11:00:28 -0700

    Add src/optimizer/__init__.py with package docstring

---
+++ src/optimizer/__init__.py
@@ -0,0 +1,6 @@
+"""
+Optimizer package for computing optimal multi-leg combinations.
+
+This subpackage contains classes and functions for evaluating and optimizing
+parlay combinations based on expected value and model probabilities.
+"""
